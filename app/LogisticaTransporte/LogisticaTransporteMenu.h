542c0f64bd8ce1b0ff9c785a5c011f82fd3352eb
Luis Lopez <luis.dev.master@gmail.com>
2024-05-23 12:07:16 -0600

    Creating menu for LogisticaTransporte module

---
+++ app/LogisticaTransporte/LogisticaTransporteMenu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class LogisticaTransporteMenu
+{
+public:
+    
+};
--- app/app.cpp
+++ app/app.cpp
@@ -2,8 +2,47 @@
 
 using namespace std;
 
+int option;
+
 int main() {
 
-    cout << "Menu";
+    cout << "===============================================================\n";
+
+    cout << " BIENVENIDO AL SISTEMA DE TRAZABILIDAD DE FRUTAS EXPORTADAS \n";
+
+    cout << "===============================================================\n";
+
+    cout << "MODULOS: " << endl;
+
+    cout << "1. COSECHA Y PRODUCCION " << endl;
+
+    cout << "2. CONTROL DE CALIDAD " << endl;
+
+    cout << "3. LOGISTICA Y TRANSPORTE" << endl;
+
+    cout << "4. DISTRIBUCION " << endl;
+
+    cout << "SELECCIONE UNA OPCION: ";
+
+    cin >> option;
+
+    switch (option)
+    {
+        case 1:
+            cout << "COSECHA Y PRODUCCION";
+            break;
+        case 2:
+            cout << "CONTROL DE CALIDAD";
+            break;
+        case 3:
+            cout << "LOGISTICA Y TRANSPORTE";
+            break;
+        case 4:
+            cout <<  "DISTRIBUCION";   
+            break;
+        default:
+            cout << "OPCION INVALIDA";
+    }
+    
     return 0;
 }
