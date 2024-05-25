d00a3f97bd2dacb362094c0e566df174e7782122
Luis Lopez <luis.dev.master@gmail.com>
2024-05-25 08:46:22 -0600

    Commenting BodegaAlimentos model

--- app/ControlCalidad/Business/Models/BodegaAlimentos.h
+++ app/ControlCalidad/Business/Models/BodegaAlimentos.h
@@ -1,12 +1,35 @@
 #pragma once
 
+/**
+ * @brief Clase que representa una bodega de alimentos.
+ */
 class BodegaAlimentos
 {
 public:
-    int id;
-    int plantaId;
+    int id; ///< Identificador de la bodega.
+    int plantaId;  ///< Identificador de la planta a la que pertenece la bodega.
+
+    /**
+     * @brief Obtiene el identificador de la bodega.
+     * @return El identificador de la bodega.
+     */
     int get_id() const;
+
+    /**
+    * @brief Establece el identificador de la bodega.
+    * @param id El nuevo identificador de la bodega.
+    */
     void set_id(int id);
+
+    /**
+     * @brief Obtiene el identificador de la planta a la que pertenece la bodega.
+     * @return El identificador de la planta.
+     */
     int get_planta_id() const;
+
+   /**
+    * @brief Establece el identificador de la planta a la que pertenece la bodega.
+    * @param planta_id El nuevo identificador de la planta.
+    */
     void set_planta_id(int planta_id);
 };
